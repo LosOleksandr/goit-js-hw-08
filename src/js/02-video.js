f6c67ac73440a315c74e4946af20884693195219
LosOleksandr <losalexander2003@gmail.com>
2023-04-20 13:58:09 +0300

    summary

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, currentTime);
+};
+
+const storedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player
+  .setCurrentTime(storedTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'Range error':
+        break;
+    }
+  });
+
+player.setVolume(0.5);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+window.addEventListener('keydown', evt => {
+  if (evt.code === 'Space') player.pause();
+});
+
+window.addEventListener('keydown', evt => {
+  if (evt.code === 'KeyF') player.getFullscreen();
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+
+const LOCALESTORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInputForm, 500));
+
+function onInputForm() {
+  const feedback = getFormValues(formEl);
+  localStorage.setItem(LOCALESTORAGE_KEY, JSON.stringify(feedback));
+}
+
+const storedFeedback = localStorage.getItem(LOCALESTORAGE_KEY);
+const parsedFeedback = JSON.parse(storedFeedback);
+
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  onFormSubmit(evt);
+  evt.currentTarget.reset();
+});
+
+function onFormSubmit({ currentTarget }) {
+  if (localStorage.getItem(LOCALESTORAGE_KEY) === null)
+    return alert('Введіть дані у форму!');
+  localStorage.removeItem(LOCALESTORAGE_KEY);
+  const feedback = getFormValues(currentTarget);
+  console.log(feedback);
+}
+
+function checkLocalStorage(form) {
+  if (localStorage.getItem(LOCALESTORAGE_KEY) === null) return;
+  form.email.value = parsedFeedback.email;
+  form.message.value = parsedFeedback.message;
+}
+
+checkLocalStorage(formEl);
+
+function getFormValues(form) {
+  const formValues = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  return formValues;
+}
 
