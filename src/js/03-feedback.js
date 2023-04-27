5050418388e5da2bf67b52afe7e284c1a177929d
LosOleksandr <losalexander2003@gmail.com>
2023-04-27 13:07:21 +0300

    Summary

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,7 +12,17 @@ function onInputForm() {
 }
 
 const storedFeedback = localStorage.getItem(LOCALESTORAGE_KEY);
-const parsedFeedback = JSON.parse(storedFeedback);
+const parsedFeedback = getParsedFeedback(storedFeedback);
+
+function getParsedFeedback(storedValue) {
+  try {
+    const parsedValue = JSON.parse(storedValue);
+    return parsedValue;
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
 
 formEl.addEventListener('submit', evt => {
   evt.preventDefault();
@@ -21,26 +31,22 @@ formEl.addEventListener('submit', evt => {
 });
 
 function onFormSubmit({ currentTarget }) {
-  if (localStorage.getItem(LOCALESTORAGE_KEY) === null)
+  if (!localStorage.getItem(LOCALESTORAGE_KEY))
     return alert('Введіть дані у форму!');
   localStorage.removeItem(LOCALESTORAGE_KEY);
   const feedback = getFormValues(currentTarget);
   console.log(feedback);
 }
 
-function checkLocalStorage(form) {
-  if (localStorage.getItem(LOCALESTORAGE_KEY) === null) return;
-  form.email.value = parsedFeedback.email;
-  form.message.value = parsedFeedback.message;
+function checkLocalStorage(parsedValue, form) {
+  if (!localStorage.getItem(LOCALESTORAGE_KEY)) return;
+  const formData = Object.entries(parsedValue);
+  formData.forEach((value, i) => (form.elements[i].value = value[1]));
 }
 
-checkLocalStorage(formEl);
+checkLocalStorage(parsedFeedback, formEl);
 
 function getFormValues(form) {
-  const formValues = {
-    email: form.email.value,
-    message: form.message.value,
-  };
+  const formValues = Object.fromEntries(new FormData(form));
   return formValues;
 }
-
